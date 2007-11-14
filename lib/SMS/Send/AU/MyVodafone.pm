e8429f17d14a7cfbc2e429361ae4b358aecc436c
Adam Kennedy <adamk@cpan.org>
2007-11-14 00:37:08 +0000

    Import of ADAMK/SMS-Send-AU-MyVodafone-0.03 from CPAN.
    gitpan-cpan-distribution: SMS-Send-AU-MyVodafone
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         ADAMK/SMS-Send-AU-MyVodafone-0.03.tar.gz
    gitpan-cpan-author:       ADAMK
    gitpan-cpan-maturity:     released

--- lib/SMS/Send/AU/MyVodafone.pm
+++ lib/SMS/Send/AU/MyVodafone.pm
@@ -70,7 +70,7 @@ use WWW::Mechanize ();
 
 use vars qw{$VERSION};
 BEGIN {
-	$VERSION = '0.02';
+	$VERSION = '0.03';
 }
 
 # Starting URI
@@ -313,11 +313,11 @@ For other issues, contact the author.
 
 =head1 AUTHOR
 
-Adam Kennedy E<lt>cpan@ali.asE<gt>, L<http://ali.as/>
+Adam Kennedy E<lt>adamk@cpan.orgE<gt>, L<http://ali.as/>
 
 =head1 COPYRIGHT
 
-Copyright 2005 Adam Kennedy. All rights reserved.
+Copyright 2005 Adam Kennedy.
 
 This program is free software; you can redistribute
 it and/or modify it under the same terms as Perl itself.
